fd64e16311739a25593958b04c2a46235822eee1
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2023-06-17 22:36:42 +0300

    error page completed

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -2,7 +2,13 @@ import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 const Error = () => {
-  return <h2>error page</h2>;
+  return <Wrapper>
+    <div>
+      <h1>404</h1>
+      <h3>sorry, the page tried cannot be found</h3>
+    </div>
+    <Link to="/" className='btn'>back home</Link>
+  </Wrapper>;
 };
 const Wrapper = styled.section`
   min-height: 100vh;
