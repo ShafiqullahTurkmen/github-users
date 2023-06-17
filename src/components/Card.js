af05996267869aa7f3a76abcc5d628705d5aa25a
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2023-06-18 00:06:35 +0300

    Card component updated

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,26 @@
 import React from 'react';
-import { GithubContext } from '../context/context';
+import { GithubContext, useGithubContext } from '../context/context';
 import styled from 'styled-components';
 import { MdBusiness, MdLocationOn, MdLink } from 'react-icons/md';
 const Card = () => {
-  return <h2>card component</h2>;
+  const { githubUser: { avatar_url, html_url, name, company, blog, bio, location, twitter_username } } = useGithubContext();
+
+  return <Wrapper>
+    <header>
+      <img src={avatar_url} alt={name} />
+      <div>
+        <h4>{name}</h4>
+        <p>@{twitter_username || "john doe"}</p>
+      </div>
+      <a href={html_url}>follow</a>
+    </header>
+    <p className="bio">{bio || "Software developer"}</p>
+    <div className="links">
+      <p><MdBusiness/>{name === "Shafiqullah Turkmen" ? "Alotech" : company}</p>
+      <p><MdLocationOn/>{location || "earth"}</p>
+      <a href={`https://${blog || "medium.com"}`}><MdLink/>{blog }{blog || "Medium"}</a>
+    </div>
+  </Wrapper>;
 };
 const Wrapper = styled.article`
   background: var(--clr-white);
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import { Info, Repos, User, Search, Navbar } from '../components';
 import loadingImage from '../images/preloader.gif';
-import { useGithubContext } from '../context/context';
 const Dashboard = () => {
-  const { greeting } = useGithubContext();
   return (
     <main>
-      <Navbar/>
-      <Search/>
+      {/* <Navbar/> */}
+      {/* <Search/> */}
       <Info/>
       <User/>
-      <Repos/>
-      <h1>{greeting}</h1>
+      {/* <Repos/> */}
     </main>
   );
 };
