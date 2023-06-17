1df821fd7c5771863e27ccf93ce3ad7e4e7bec52
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2023-06-17 22:26:13 +0300

    project structure added

---
+++ src/context/context.js
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from 'react';
+import mockUser from './mockData/mockUser';
+import mockRepos from './mockData/mockRepos';
+import mockFollowers from './mockData/mockFollowers';
+import axios from 'axios';
+
+const rootUrl = 'https://api.github.com';
