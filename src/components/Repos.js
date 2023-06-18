22b711ae43dc4ced74bc42d8632972a59419812a
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2023-06-18 09:05:43 +0300

    Most popular  languages calculated

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -5,25 +5,26 @@ import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
 const Repos = () => {
   const { repos } = useGithubContext()
 
-  let languages = repos.reduce((total, item) => {
-    const { language } = item;
+  const languages = repos.reduce((total, item) => {
+    const { language, stargazers_count } = item;
     if (!language) return total;
     if (!total[language]) {
-      total[language] = { label: language, value: 1 };
+      total[language] = { label: language, value: 1, stars: stargazers_count };
     } else {
-      total[language] = { ...total[language], value: total[language].value + 1 }
+      total[language] = { ...total[language], value: total[language].value + 1, stars: total[language].stars + stargazers_count }
     }
     return total
   }, {})
 
-  languages = Object.values(languages).sort((a, b) => (b.value - a.value)).slice(0, 6)
-  
+  const mostUsed = Object.values(languages).sort((a, b) => (b.value - a.value)).slice(0, 6);
+  const mostPopular = Object.values(languages).sort((a, b) => (b.stars - a.stars)).map(item => ({ ...item, value: item.stars })).slice(0, 5)
+
   return (
     <section className='section'>
       <Wrapper>
-        <Pie3D data={languages} />
+        <Pie3D data={mostUsed} />
         <div></div>
-        <Doughnut2D data={languages} />
+        <Doughnut2D data={mostPopular} />
         <div></div>
       </Wrapper>
     </section>)
